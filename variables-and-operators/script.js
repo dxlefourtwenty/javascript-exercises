0bb17fef15170e85ea5200e236a8484fc7847183
Dale <159103006+dxlefourtwenty@users.noreply.github.com>
2025-08-02 11:59:03 -0700

    added variables-and-operators folder

---
+++ variables-and-operators/script.js
@@ -0,0 +1,55 @@
+/* let name = "John"
+let surname = "Doe"
+
+console.log(name)
+console.log(surname) /* can also use 'var' to assign instead of 'let' */
+
+/* let age = 11
+console.log(age)
+
+age = 54;
+console.log(age); /* don't need ; but good practice to */
+
+/* const pi = 3.14
+
+pi = 4
+
+console.log(pi) /* throws an error */
+
+/*let number = (3 + 2) - 76 * (1 + 1)
+console.log(number);*/
+
+/** practice **/
+console.log(23 + 97);
+
+console.log((4 + 6 + 7) / 77);
+
+let a = 10
+console.log(a);
+
+a = 127
+console.log(a);
+
+let b = 7 * a
+console.log(b);
+
+const max = 57
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
+
+let s = "my" + "string"
+alert(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
