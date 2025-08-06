566799d12df09a2dde7f94fdbeccbc1ddb75c77c
Dale <159103006+dxlefourtwenty@users.noreply.github.com>
2025-08-06 14:58:03 -0700

    added solution to repeateString.js and added pre-planning file

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // let arr = [];
+    // let result = "";
 
+    // for (let i = str.length - 1; i > -1; i--) {
+    //     arr.push(str[i]);
+    // }
+    
+    // for (let char of arr) {
+    //     result += char;
+    // }
+
+    // return result;
+    return str.split("").reverse().join("");
 };
+
+console.log(reverseString("Hello World"));
 
 // Do not edit below this line
 module.exports = reverseString;
