7416401091bdda5f24dfe8c0c1f8bfa892f411c3
Dale <159103006+dxlefourtwenty@users.noreply.github.com>
2025-08-06 17:24:42 -0700

    implemented solution to leapYears.js

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear = false;
 
+    if ((year % 4 == 0) && (year % 100 != 0)) 
+        isLeapYear = true;
+    if (year % 400 == 0)
+        isLeapYear = true;
+
+    return isLeapYear;
 };
+
+console.log(leapYears(1996));
 
 // Do not edit below this line
 module.exports = leapYears;
