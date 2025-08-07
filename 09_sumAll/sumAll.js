77264e4e4f5d9bb1ca5eaa4cc4f5caa539a02d2b
Dale <159103006+dxlefourtwenty@users.noreply.github.com>
2025-08-06 17:16:53 -0700

    implemented solution to 'sumALL.js'

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x > y) {
+        let temp = x;
+        x = y;
+        y = temp;
+    }
+    if (x < 0 || y < 0)
+        return "ERROR";
+    if (x % 1 != 0 || y % 1 != 0)
+        return "ERROR";
+    if (typeof x === "string" || typeof y === "string")
+        return "ERROR";
 
+    let array = [];
+    for (let i = x; i < y + 1; i++) {
+        array.push(i);
+    }
+    let finalSum = array.reduce((total, currentValue) => {
+        return total + currentValue;
+    });
+
+    return finalSum;
 };
 
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
