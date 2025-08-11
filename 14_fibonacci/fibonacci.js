89680e0c970527e48e4743ab4d6375e0e9a1cf82
Dale <159103006+dxlefourtwenty@users.noreply.github.com>
2025-08-10 19:58:59 -0700

    implemented solution to fibonacci.js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (typeof 'string') {
+        index = parseInt(index);
+    }
 
+    if (index === 0) {
+        return 0;
+    } else if (index == 1) {
+        return 1;
+    } else if (index < 0) {
+        return 'OOPS';
+    }
+
+    let prev = 0;
+    let curr = 1;
+
+    for (let i = 2; i < index + 1; i++) {
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+
+    return curr;
 };
 
+
+// n + prev
 // Do not edit below this line
 module.exports = fibonacci;
