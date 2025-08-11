70276621369015e377acfd3eb4de55ff20ac3378
Dale <159103006+dxlefourtwenty@users.noreply.github.com>
2025-08-10 19:26:03 -0700

    implemented solution to palindromes.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function(input) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedString = input
+    .toLowerCase().split('')
+    .filter((char) => alphanumerical.includes(char))
+    .join('');
+
+    const reversedString = cleanedString.split('').reverse('').join('');
+
+    return reversedString === cleanedString;
 };
 
 // Do not edit below this line
