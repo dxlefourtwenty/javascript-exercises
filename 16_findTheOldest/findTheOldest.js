956a5f5d0171ba61391c9dec04b283e0123990ac
Dale <159103006+dxlefourtwenty@users.noreply.github.com>
2025-08-10 21:18:03 -0700

    implemented solution to findTheOldest.js

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,48 @@
-const findTheOldest = function() {
+// const findTheOldest = function(array) {
+//     const now = new Date().getFullYear();
+//     let ages = [];
+//     let i = 0;
+//     let max = 0;
+//     let oldest = '';
+//     array.map(person => {
+//         if (person.yearOfDeath === undefined) {
+//             person.yearOfDeath = now;
+//         }
+//         let birthYear = person.yearOfBirth;
+//         let deathYear = person.yearOfDeath;
+//         ages[i] = deathYear - birthYear;
+//         if (max < ages[i]) {
+//             max = ages[i];
+//             oldest = person.name;
+//         }
+//         i++;
+//     });
 
-};
+//     return oldest;
+// };
+
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if (oldestAge < currentAge) {
+            return currentPerson;
+        } else {
+            return oldest;
+        }
+    });
+}
+
+const getAge = function(birth, death) {
+    const now = new Date().getFullYear();
+    if (!death) {
+        death = now;
+    }
+
+    return death - birth;
+}
+
+//console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
